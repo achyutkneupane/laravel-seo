281837f8147faf13d2d28694b9dde2f74684d765
achyutkneupane <achyutkneupane@gmail.com>
2025-10-12 00:48:17 +0545

    chore: adds scripts section to composer.json

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,30 +4,10 @@ declare(strict_types=1);
 
 namespace AchyutN\LaravelSEO\Tests;
 
-use AchyutN\FilamentLogViewer\LogViewerProvider;
-use AchyutN\FilamentLogViewer\Tests\Providers\TestPanelProvider;
 use AllowDynamicProperties;
-use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
-use BladeUI\Icons\BladeIconsServiceProvider;
-use Filament\Actions\ActionsServiceProvider;
-use Filament\FilamentServiceProvider;
-use Filament\Forms\FormsServiceProvider;
-use Filament\Infolists\InfolistsServiceProvider;
-use Filament\Notifications\NotificationsServiceProvider;
-use Filament\Schemas\SchemasServiceProvider;
-use Filament\Support\SupportServiceProvider;
-use Filament\Tables\TablesServiceProvider;
-use Filament\Widgets\WidgetsServiceProvider;
-use FilesystemIterator;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
-use Illuminate\Log\LogServiceProvider;
-use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as BaseTestCase;
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
-use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
 
 #[AllowDynamicProperties]
 abstract class TestCase extends BaseTestCase
