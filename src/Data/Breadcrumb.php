dc167b913839cb1599fdd86c9eb2ff33769cab37
achyutkneupane <achyutkneupane@gmail.com>
2026-01-04 16:06:11 +0545

    refactor: updates Breadcrumb class to be readonly

--- src/Data/Breadcrumb.php
+++ src/Data/Breadcrumb.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace AchyutN\LaravelSEO\Data;
 
-final class Breadcrumb
+final readonly class Breadcrumb
 {
     public function __construct(
         private string $label,
