508c7190f9a073434ac76fd9a5dfcabf4e64da5c
achyutkneupane <achyutkneupane@gmail.com>
2026-01-04 15:47:39 +0545

    feat: adds type hints for schema and SEO data in HasMarkup interface

--- src/Contracts/HasMarkup.php
+++ src/Contracts/HasMarkup.php
@@ -7,8 +7,15 @@ namespace AchyutN\LaravelSEO\Contracts;
 use AchyutN\LaravelSEO\Data\ResolvedSEO;
 use RalphJSmit\Laravel\SEO\SchemaCollection;
 
+/**
+ * @template TKey of array-key
+ */
 interface HasMarkup
 {
+    /**
+     * @param  SchemaCollection<TKey>  $schema
+     * @return SchemaCollection<TKey>
+     */
     public function buildSchema(
         SchemaCollection $schema,
         ResolvedSEO $seo
