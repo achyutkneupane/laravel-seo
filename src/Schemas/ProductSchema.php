d5e018ad88d4b55bea7a16fa87e20262a49fe4ff
achyutkneupane <achyutkneupane@gmail.com>
2026-01-04 15:18:27 +0545

    feat: adds ProductSchema trait for structured product data

---
+++ src/Schemas/ProductSchema.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AchyutN\LaravelSEO\Schemas;
+
+use AchyutN\LaravelSEO\Data\ResolvedSEO;
+use RalphJSmit\Laravel\SEO\SchemaCollection;
+
+trait ProductSchema
+{
+    protected function productSchemaType(): string
+    {
+        return 'Product';
+    }
+
+    public function buildSchema(SchemaCollection $schema, ResolvedSEO $resolvedSEO): SchemaCollection
+    {
+        return $schema
+            ->add(fn (): array => [
+                '@context' => 'https://schema.org',
+                '@type' => $resolvedSEO->pageType ?? $this->productSchemaType(),
+                'name' => $resolvedSEO->title,
+                'description' => $resolvedSEO->description,
+                'url' => $resolvedSEO->url,
+                'image' => $resolvedSEO->image,
+                'brand' => $resolvedSEO->brandArray(),
+                'sku' => $resolvedSEO->sku,
+                'offers' => [
+                    '@type' => 'Offer',
+                    'priceCurrency' => $resolvedSEO->currency,
+                    'price' => $resolvedSEO->price,
+                    'availability' => sprintf(
+                        'https://schema.org/%s',
+                        $resolvedSEO->isAvailable ? 'InStock' : 'OutOfStock'
+                    ),
+                ],
+            ]);
+    }
+}
