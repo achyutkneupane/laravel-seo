ff2bc8fec7c09ea9ad61f4ab261f9618ad65dc7a
achyutkneupane <achyutkneupane@gmail.com>
2026-01-22 23:11:55 +0545

    refactor: simplifies schema generation logic in BlogSchema Improves readability and maintainability by using collection methods for conditional attributes.

--- src/Schemas/BlogSchema.php
+++ src/Schemas/BlogSchema.php
@@ -18,26 +18,49 @@ trait BlogSchema
         $resolvedSEO = $this->resolveSEO();
 
         return $schema
-            ->add(fn (): array => [
-                '@context' => 'https://schema.org',
-                '@type' => $resolvedSEO->pageType ?? $this->blogSchemaType(),
-                'headline' => $resolvedSEO->title,
-                'description' => $resolvedSEO->description,
-                'url' => $resolvedSEO->url,
-                'thumbnailUrl' => $resolvedSEO->image,
-                'articleSection' => $resolvedSEO->category,
-                'datePublished' => $resolvedSEO->publishedAt,
-                'inLanguage' => 'en',
-                'author' => $resolvedSEO->authorAndPublisher(),
-            ])
-            ->addArticle(fn (ArticleSchema $articleSchema): ArticleSchema => $articleSchema->markup(fn (Collection $markup): Collection => $markup
-                ->put('headline', $resolvedSEO->title)
-                ->put('description', $resolvedSEO->description)
-                ->put('url', $resolvedSEO->url)
-                ->put('thumbnailUrl', $resolvedSEO->image)
-                ->put('author', $resolvedSEO->authorAndPublisher())
-                ->put('datePublished', $resolvedSEO->publishedAt)
-            ));
+            ->add(
+                fn () => collect()
+                    ->put('@context', 'https://schema.org')
+                    ->put('@type', $resolvedSEO->pageType ?? $this->blogSchemaType())
+                    ->put('headline', $resolvedSEO->title)
+                    ->put('inLanguage', 'en')
+                    ->when(
+                        $resolvedSEO->description,
+                        fn (Collection $collection) => $collection->put('description', $resolvedSEO->description)
+                    )
+                    ->when(
+                        $resolvedSEO->url,
+                        fn (Collection $collection) => $collection->put('url', $resolvedSEO->url)
+                            ->put('@id', $resolvedSEO->url)
+                    )
+                    ->when(
+                        $resolvedSEO->image,
+                        fn (Collection $collection) => $collection->put('thumbnailUrl', $resolvedSEO->image)
+                    )
+                    ->when(
+                        $resolvedSEO->category,
+                        fn (Collection $collection) => $collection->put('articleSection', $resolvedSEO->category)
+                    )
+                    ->when(
+                        $resolvedSEO->publishedAt,
+                        fn (Collection $collection) => $collection->put('datePublished', $resolvedSEO->publishedAt)
+                    )
+                    ->put('author', $resolvedSEO->authorAndPublisher())
+            )
+            ->addArticle(
+                function (ArticleSchema $articleSchema) use ($resolvedSEO): ArticleSchema {
+                    return $articleSchema->markup(
+                        fn (Collection $markup): Collection => $markup
+                            ->put('headline', $resolvedSEO->title)
+                            ->put('description', $resolvedSEO->description)
+                            ->put('url', $resolvedSEO->url)
+                            ->put('thumbnailUrl', $resolvedSEO->image)
+                            ->put('articleSection', $resolvedSEO->category)
+                            ->put('author', $resolvedSEO->authorAndPublisher())
+                            ->put('datePublished', $resolvedSEO->publishedAt)
+                    );
+                }
+            );
     }
 
     protected function blogSchemaType(): string
