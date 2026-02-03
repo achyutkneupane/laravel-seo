b529d5ff1b490e30472768cbc68a636c522446ff
achyutkneupane <achyutkneupane@gmail.com>
2026-02-03 16:34:12 +0545

    feat: extends HasMarkup interface to include HasColumns methods

--- src/Contracts/HasColumns.php
+++ src/Contracts/HasColumns.php
@@ -9,22 +9,41 @@ use Illuminate\Support\Carbon;
 interface HasColumns
 {
     public function getTitleValue(): ?string;
+
     public function getDescriptionValue(): ?string;
+
     public function getCategoryValue(): ?string;
+
     public function getImageValue(): ?string;
+
     public function getAuthorValue(): ?string;
+
     public function getAuthorUrlValue(): ?string;
+
     public function getPublisherValue(): ?string;
+
     public function getPublisherUrlValue(): ?string;
+
+    /** @return array<int, string>|null */
     public function getTagsValue(): ?array;
+
     public function getURLValue(): ?string;
+
     public function getPublishedAtValue(): ?Carbon;
+
     public function getModifiedAtValue(): ?Carbon;
+
     public function getPageTypeValue(): ?string;
+
     public function getBrandValue(): ?string;
+
     public function getPriceValue(): ?float;
+
     public function getdiscountPriceValue(): ?float;
+
     public function getCurrencyValue(): ?string;
+
     public function getAvailabilityValue(): bool;
+
     public function getSkuValue(): ?string;
 }
