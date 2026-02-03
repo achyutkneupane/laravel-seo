500535e146643c3fa7e903782fcbdd0488fd9db6
achyutkneupane <achyutkneupane@gmail.com>
2026-02-03 16:30:59 +0545

    feat: adds HasColumns interface for SEO-related attributes

---
+++ src/Contracts/HasColumns.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AchyutN\LaravelSEO\Contracts;
+
+use Illuminate\Support\Carbon;
+
+interface HasColumns
+{
+    public function getTitleValue(): ?string;
+    public function getDescriptionValue(): ?string;
+    public function getCategoryValue(): ?string;
+    public function getImageValue(): ?string;
+    public function getAuthorValue(): ?string;
+    public function getAuthorUrlValue(): ?string;
+    public function getPublisherValue(): ?string;
+    public function getPublisherUrlValue(): ?string;
+    public function getTagsValue(): ?array;
+    public function getURLValue(): ?string;
+    public function getPublishedAtValue(): ?Carbon;
+    public function getModifiedAtValue(): ?Carbon;
+    public function getPageTypeValue(): ?string;
+    public function getBrandValue(): ?string;
+    public function getPriceValue(): ?float;
+    public function getdiscountPriceValue(): ?float;
+    public function getCurrencyValue(): ?string;
+    public function getAvailabilityValue(): bool;
+    public function getSkuValue(): ?string;
+}
