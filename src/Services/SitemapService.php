3721e87c177082b33cdd8c96a13c420de0b21dd0
achyutkneupane <achyutkneupane@gmail.com>
2026-05-21 01:50:15 +0545

    feat(sitemap): render multiple images without captions and add YouTube video support

--- src/Services/SitemapService.php
+++ src/Services/SitemapService.php
@@ -24,7 +24,7 @@ final class SitemapService
         $xml = [];
 
         $xml[] = '<?xml version="1.0" encoding="UTF-8"?>';
-        $xml[] = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">';
+        $xml[] = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">';
 
         foreach ($seoModels as $seoModel) {
             $model = $seoModel->model;
@@ -39,6 +39,8 @@ final class SitemapService
                 'title' => $title,
                 'description' => $description,
                 'updatedAt' => $updatedAt,
+                'sitemapImages' => $sitemapImages,
+                'sitemapVideos' => $sitemapVideos,
             ] = $this->service->getModelValues($model);
 
             if ($url === null) {
@@ -51,12 +53,62 @@ final class SitemapService
                 $xml[] = '<lastmod>'.htmlspecialchars($updatedAt->toAtomString(), ENT_XML1, 'UTF-8').'</lastmod>';
             }
 
-            if ($imageUrl) {
+            if ($imageUrl && empty($sitemapImages)) {
                 $xml[] = '<image:image>';
                 $xml[] = '<image:loc>'.htmlspecialchars($imageUrl, ENT_XML1, 'UTF-8').'</image:loc>';
                 $xml[] = '<image:title>'.htmlspecialchars($title ?? '', ENT_XML1, 'UTF-8').'</image:title>';
                 $xml[] = '<image:caption>'.htmlspecialchars($description ?? '', ENT_XML1, 'UTF-8').'</image:caption>';
                 $xml[] = '</image:image>';
+            }
+
+            foreach ($sitemapImages as $sitemapImageUrl) {
+                $xml[] = '<image:image>';
+                $xml[] = '<image:loc>'.htmlspecialchars($sitemapImageUrl, ENT_XML1, 'UTF-8').'</image:loc>';
+                if (count($sitemapImages) === 1) {
+                    $xml[] = '<image:title>'.htmlspecialchars($title ?? '', ENT_XML1, 'UTF-8').'</image:title>';
+                    $xml[] = '<image:caption>'.htmlspecialchars($description ?? '', ENT_XML1, 'UTF-8').'</image:caption>';
+                }
+                $xml[] = '</image:image>';
+            }
+
+            foreach ($sitemapVideos as $video) {
+                /** @var array{thumbnail_loc: string, title: string, description: string, content_loc?: string, player_loc?: string, duration?: int|float|string, publication_date?: string, expiration_date?: string, rating?: int|float|string, view_count?: int|float|string, family_friendly?: bool, requires_subscription?: bool, live?: bool} $video */
+                $xml[] = '<video:video>';
+                $xml[] = '<video:thumbnail_loc>'.htmlspecialchars($video['thumbnail_loc'], ENT_XML1, 'UTF-8').'</video:thumbnail_loc>';
+                $xml[] = '<video:title>'.htmlspecialchars($video['title'], ENT_XML1, 'UTF-8').'</video:title>';
+                $xml[] = '<video:description>'.htmlspecialchars($video['description'], ENT_XML1, 'UTF-8').'</video:description>';
+
+                if (isset($video['content_loc'])) {
+                    $xml[] = '<video:content_loc>'.htmlspecialchars($video['content_loc'], ENT_XML1, 'UTF-8').'</video:content_loc>';
+                }
+                if (isset($video['player_loc'])) {
+                    $xml[] = '<video:player_loc>'.htmlspecialchars($video['player_loc'], ENT_XML1, 'UTF-8').'</video:player_loc>';
+                }
+                if (isset($video['duration'])) {
+                    $xml[] = '<video:duration>'.(int) $video['duration'].'</video:duration>';
+                }
+                if (isset($video['publication_date'])) {
+                    $xml[] = '<video:publication_date>'.htmlspecialchars($video['publication_date'], ENT_XML1, 'UTF-8').'</video:publication_date>';
+                }
+                if (isset($video['expiration_date'])) {
+                    $xml[] = '<video:expiration_date>'.htmlspecialchars($video['expiration_date'], ENT_XML1, 'UTF-8').'</video:expiration_date>';
+                }
+                if (isset($video['rating'])) {
+                    $xml[] = '<video:rating>'.(float) $video['rating'].'</video:rating>';
+                }
+                if (isset($video['view_count'])) {
+                    $xml[] = '<video:view_count>'.(int) $video['view_count'].'</video:view_count>';
+                }
+                if (isset($video['family_friendly'])) {
+                    $xml[] = '<video:family_friendly>'.($video['family_friendly'] ? 'yes' : 'no').'</video:family_friendly>';
+                }
+                if (isset($video['requires_subscription'])) {
+                    $xml[] = '<video:requires_subscription>'.($video['requires_subscription'] ? 'yes' : 'no').'</video:requires_subscription>';
+                }
+                if (isset($video['live'])) {
+                    $xml[] = '<video:live>'.($video['live'] ? 'yes' : 'no').'</video:live>';
+                }
+                $xml[] = '</video:video>';
             }
             $xml[] = '</url>';
         }
